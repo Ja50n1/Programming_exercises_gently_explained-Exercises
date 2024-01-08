67c510119585bdad0e08318806e48278074aaf5f
Ja5On1 <jason.dennis.1@gmail.com>
2024-01-08 08:02:41 -0500

    finished ex16 mode.py

--- ex16-Mode/mode.py
+++ ex16-Mode/mode.py
@@ -1,14 +1,24 @@
 def mode(numbers):
-    pass
+    if len(numbers) == 0:
+        return None
+    num_occur = {}
+    most_frequent_num = numbers[0]
+    for i in numbers:
+        num_occur[i] = num_occur.get(i, 0) + 1
+        if num_occur[i] > num_occur[most_frequent_num]:
+            most_frequent_num = i
+    return most_frequent_num
 
 
 def main():
     assert mode([]) == None
     assert mode([1, 2, 3, 4, 4]) == 4
     assert mode([1, 1, 2, 3, 4]) == 1
+
     import random
 
     random.seed(42)
+
     testData = [1, 2, 3, 4, 4]
     for i in range(1000):
         random.shuffle(testData)
