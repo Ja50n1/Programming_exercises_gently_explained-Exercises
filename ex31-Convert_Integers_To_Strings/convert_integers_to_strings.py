715e394e6b4d3aae8304daa0cbad4c29d77595a8
Ja5On1 <jason.dennis.1@gmail.com>
2024-01-10 17:32:23 -0500

    ex31 completed ... ex32 convert_strings_to_integers.py templated only

--- ex31-Convert_Integers_To_Strings/convert_integers_to_strings.py
+++ ex31-Convert_Integers_To_Strings/convert_integers_to_strings.py
@@ -1,9 +1,41 @@
 def convertIntToStr(integerNum):
-    pass
+    # create dict keys 0-9 with equivalent string value
+    digit_to_char = {}
+    for k, v in enumerate(range(10)):
+        digit_to_char[k] = chr(48 + v)
+
+    string_digits = ""
+    negative = False
+
+    # check for 0
+    if integerNum == 0:
+        return "0"
+
+    # check for negative
+    if integerNum < 0:
+        integerNum = abs(integerNum)
+        negative = True
+
+    # create number string
+    while integerNum != 0:
+        working_digit = integerNum % 10
+        string_digits = digit_to_char[working_digit] + string_digits
+        integerNum = integerNum // 10
+
+    # add negative if needed
+    if negative:
+        string_digits = "-" + string_digits
+
+    return string_digits
 
 
 def main():
-    pass
+    for i in range(-10000, 10000):
+        assert convertIntToStr(i) == str(i)
+    print(convertIntToStr(57))
+    print(convertIntToStr(987057))
+    print(convertIntToStr(-57))
+    print(convertIntToStr(0))
 
 
 if __name__ == "__main__":
---
+++ ex32-Convert_Strings_to_Integers/convert_strings_to_integers.py
@@ -0,0 +1,10 @@
+def convertStrToInt(stringNum):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
